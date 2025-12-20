58dc868d2d049e45920b02c759301d77d92bc748
Gibson Rolemberg <gibsonrsf@gmail.com>
2025-12-20 14:48:53 -0300

    adicionado a dag para testar o R e o Hop

---
+++ dags/04_orchestracao_hop_r.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+# Recursos para garantir que não trave sua máquina local
+resources_config = {
+    'request_cpu': '200m',
+    'request_memory': '256Mi',
+    'limit_cpu': '1',
+    'limit_memory': '1Gi'
+}
+
+with DAG(
+    '04_orchestracao_hop_r',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=['poc', 'hop', 'r', 'k8s']
+) as dag:
+
+    # --- TAREFA 1: EXECUTAR SCRIPT R ---
+    # O Airflow cria um pod isolado só para isso.
+    # Usamos a imagem "rocker/r-base" que já vem com R pronto.
+    tarefa_r = KubernetesPodOperator(
+        task_id="analise_r",
+        name="pod-analise-r",
+        namespace="airflow", 
+        image="rocker/r-base:latest",
+        # Comando: Rscript -e "print(...)"
+        cmds=["Rscript", "-e", "print('SUCESSO: O R esta rodando via KubernetesPodOperator!')"],
+        is_delete_operator_pod=True, # Deleta o pod ao final para não sujar o cluster
+        get_logs=True, # Tenta pegar o log para mostrar na UI
+        resources=resources_config,
+        image_pull_policy="Always" # Garante que baixa a imagem se não tiver
+    )
+
+    # --- TAREFA 2: EXECUTAR APACHE HOP ---
+    # O Airflow cria outro pod isolado.
+    # Usamos a imagem "apache/hop" que já vem com Java e Hop prontos.
+    # --- TAREFA 2: EXECUTAR APACHE HOP ---
+    tarefa_hop = KubernetesPodOperator(
+        task_id="workflow_hop",
+        name="pod-workflow-hop",
+        namespace="airflow",
+        image="apache/hop:2.11.0",
+        # CORREÇÃO: Vamos rodar direto. Se falhar, queremos ver o VERMELHO.
+        cmds=["/bin/bash", "-c", "/opt/hop/hop-run.sh --version"], 
+        is_delete_operator_pod=True,
+        get_logs=True,
+        resources=resources_config,
+        image_pull_policy="Always"
+    )
+
+    # Define a ordem: Primeiro o R, depois o Hop
+    tarefa_r >> tarefa_hop
